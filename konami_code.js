044e2dbcf0079e106b731523a4106472abf02dec
Thomas Casady <tscasady@gmail.com>
2018-02-20 20:20:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,8 @@
 
 function init() {
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  let index = 0
+
     const main = document.body
     main.addEventListener('keydown', function(e) {
       var key = parseInt(e.which)
