44b3d0712acede43534669dfd3cfdc3c6c01fb6f
Thomas Casady <tscasady@gmail.com>
2018-02-20 20:27:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,18 @@
-
 function init() {
-  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   let index = 0
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  document.body.addEventListener('keydown', function (e){
+    var key = parseInt(e.which ||e.detail);
+    if (key === code[index]) {
+      console.log(index)
+      index = index + 1
+    }
+    if (index === code.length) {
+      alert("Hurray");
+      console.log('am i called?')
+      index = 0;
 
-    const main = document.body
-    main.addEventListener('keydown', function(e) {
-      var key = parseInt(e.which)
-      if (key === code[index]) {
-        index = index + 1
-      }
-      if (index === code.length) {
-        alert("Hurray");
-        index = 0;
+    }
+  })
 
-      } 
-    })
 }
