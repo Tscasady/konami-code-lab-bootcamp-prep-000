3db0a90c357cfe373eafa6ee5f4ff9f7178f4ff1
Thomas Casady <tscasady@gmail.com>
2018-02-20 19:45:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0
+
 
 function init() {
+    const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+    let index = 0
     const main = document.body
     main.addEventListener('keydown', function onKeyDownHandler(e) {
       const key = parseInt(e.which);
